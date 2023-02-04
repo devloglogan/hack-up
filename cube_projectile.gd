a5b9f2907dc2f2e6156d2aeacfce9ddf582b9e83
Logan Lang <devloglogan@gmail.com>
2023-02-04 13:04:08 -0600

    started spinning cube projectile

--- cube_projectile.gd
+++ cube_projectile.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
+@export var rotation_rate = .75
+
 @onready var cube = $Cube
 @onready var inner_cube = $InnerCube
 
 func _physics_process(delta):
-	cube.rotate_x(PI * delta)
+	cube.rotate_x(PI * rotation_rate * delta)
+	cube.rotate_y(PI * rotation_rate * delta)
+	inner_cube.rotate_x(-PI * rotation_rate * delta)
+	inner_cube.rotate_z(-PI * rotation_rate * delta)
