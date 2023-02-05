1de81c4ed555023bd40c6869b9abd0e3980987b3
Logan Lang <devloglogan@gmail.com>
2023-02-04 21:40:48 -0600

    added more keystroke sounds

--- Sound/keystroke_sound.gd
+++ Sound/keystroke_sound.gd
@@ -1,7 +1,9 @@
 extends AudioStreamPlayer
 
+@export var keystroke_sounds: Array[AudioStream]
 
 func _ready():
+	stream = keystroke_sounds.pick_random()
 	pitch_scale = randf_range(.9, 1.1)
 	play()
 
