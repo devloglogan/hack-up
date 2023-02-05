eac4f4c0be18011489f78e11f2e438aff619a10f
Logan Lang <devloglogan@gmail.com>
2023-02-05 12:11:32 -0600

    changed projectile sound

--- Objects/projectile.gd
+++ Objects/projectile.gd
@@ -6,6 +6,12 @@ const LIFETIME = 180
 var direction = Vector3()
 var countdown := LIFETIME
 
+@onready var projectile_sound = $ProjectileSound
+
+func _ready():
+	projectile_sound.doppler_tracking = AudioStreamPlayer3D.DOPPLER_TRACKING_PHYSICS_STEP
+	projectile_sound.attenuation_filter_db = -12
+
 func _physics_process(delta):
 	position += direction * SPEED * delta
 
